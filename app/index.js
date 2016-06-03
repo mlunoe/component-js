d4d5b6c7e3d937295b2f99b2055bf1f307f5af91
mlunoe <michael.lunoe@gmail.com>
2016-06-03 09:24:21 -0700

    Passing down props instead of component

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 require('./index.scss');
 var App = require('./App');
-var Image = require('./components/Image/Image');
 var RequestUtil = require('./utils/RequestUtil');
 
 window.onload = function () {
@@ -10,10 +9,7 @@ window.onload = function () {
   // Declare counter
   RequestUtil.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json', 'jsonFlickrFeed', function (photos) {
     children = photos.items.map(function (photo) {
-      return {
-        component: new Image(),
-        props: {src: photo.media.m}
-      };
+      return {src: photo.media.m}
     });
 
     app.render(container, {children: children});
