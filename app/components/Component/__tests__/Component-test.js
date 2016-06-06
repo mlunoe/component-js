ff644d4df1b0efbd28b0f92805f9e237d9b1e2e0
mlunoe <michael.lunoe@gmail.com>
2016-06-06 01:00:28 -0700

    Adding tests for component and ObjectUtil

---
+++ app/components/Component/__tests__/Component-test.js
@@ -0,0 +1,101 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var ObjectUtil = require('../../../utils/ObjectUtil');
+var Component = require('../Component');
+
+describe('Component', function () {
+  var div;
+  var component;
+
+  beforeEach(function () {
+    component = ObjectUtil.inherits({
+      componentDidMount: sinon.spy(),
+      componentWillUnmount: sinon.spy(),
+      getView: function () {
+        return '<div class="component">My Component</div>'
+      }
+    }, Component);
+    div = document.createElement('div')
+    component.render(div);
+  });
+
+  afterEach(function () {
+    component.unmount();
+  });
+
+  describe('#render()', function () {
+
+    it('renders correctly', function () {
+      var elements = div.getElementsByClassName('component');
+      expect(elements[0].innerHTML).to.equal('My Component');
+    });
+
+    it('calls replaceChild on consecutive renders', function () {
+      div.replaceChild = sinon.spy();
+      component.render(div);
+      component.render(div);
+      expect(div.replaceChild.calledOnce).to.equal(true);
+    });
+
+    it('shouldn\'t call replaceChild when rendered to new parent', function () {
+      component.render(div);
+      div.replaceChild = sinon.spy();
+      component.render(document.createElement('div'));
+      expect(div.replaceChild.called).to.equal(false);
+    });
+
+    it('calls appendChild on first render', function () {
+      div.appendChild = sinon.spy();
+      component = ObjectUtil.inherits({
+        getView: function () {
+          return '<div class="component"></div>'
+        }
+      }, Component);
+      component.render(div);
+      expect(div.appendChild.calledOnce).to.equal(true);
+    });
+
+    it('calls appendChild after unmount', function () {
+      component.render(div);
+      div.appendChild = sinon.spy();
+      component.unmount();
+      component.render(div);
+      expect(div.appendChild.calledOnce).to.equal(true);
+    });
+
+    it('shouldn\'t call appendChild on consecutive renders', function () {
+      div.appendChild = sinon.spy();
+      component.render(div);
+      expect(div.appendChild.called).to.equal(false);
+    });
+
+    it('calls componentDidMount if implemented', function () {
+      expect(component.componentDidMount.calledOnce).to.equal(true);
+    });
+
+    it('passes through arguments to componentDidMount', function () {
+      component.render(div, 'one', 'two', 'three');
+      expect(component.componentDidMount.getCall(1).args[1]).to.equal('one')
+      expect(component.componentDidMount.getCall(1).args[2]).to.equal('two')
+      expect(component.componentDidMount.getCall(1).args[3]).to.equal('three')
+    });
+
+  });
+
+  describe('#unmount()', function () {
+
+    it('removes child correctly', function () {
+      component.unmount();
+      var elements = div.getElementsByClassName('component');
+      expect(elements.length).to.equal(0);
+    });
+
+    it('calls componentWillUnmount if implemented', function () {
+      component.unmount();
+      expect(component.componentWillUnmount.calledOnce).to.equal(true);
+    });
+
+  });
+
+});
---
+++ app/utils/__tests__/ObjectUtil-test.js
@@ -0,0 +1,95 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var ObjectUtil = require('../ObjectUtil');
+
+describe('ObjectUtil', function () {
+
+  describe('#assign()', function () {
+
+    it('error cases', function () {
+      expect(function () { ObjectUtil.assign(null); }).to.throw();
+      expect(function () { ObjectUtil.assign(undefined); }).to.throw();
+      expect(function () { ObjectUtil.assign(null, {}); }).to.throw();
+      expect(function () { ObjectUtil.assign(undefined, {}); }).to.throw();
+    });
+
+    it('merges objects', function () {
+      expect(ObjectUtil.assign({a: 'a'}, {b: 'b'})).to.deep.equal({a: 'a', b: 'b'});
+    });
+
+    it('overrides propeties of target', function () {
+      expect(ObjectUtil.assign({a: 'a'}, {a: 'b'})).to.deep.equal({a: 'b'});
+    });
+
+    it('copies over undefined values', function () {
+      expect(ObjectUtil.assign({a: 'a'}, {a: undefined})).to.deep.equal({a: undefined});
+    });
+
+  });
+
+  describe('#inherits()', function () {
+
+    describe('objects', function () {
+
+      it('creates a new object', function () {
+        var a = {a: 'a'};
+        var b = {b: 'b'};
+        expect(ObjectUtil.inherits(a, b)).to.not.equal(a);
+        expect(ObjectUtil.inherits(a, b)).to.not.equal(b);
+      });
+
+      it('makes b available in the prototype', function () {
+        var a = {a: 'a'};
+        var b = {b: 'b'};
+        expect(ObjectUtil.inherits(a, b).__proto__).to.deep.equal(b);
+      });
+
+      it('makes properties available from both objects', function () {
+        var a = {a: 'a'};
+        var b = {b: 'b'};
+        expect(ObjectUtil.inherits(a, b)).to.deep.equal({a: 'a', b: 'b'});
+      });
+
+      it('prefers properties from child object', function () {
+        var a = {a: 'a', shared: 'a\'s shared'};
+        var b = {b: 'b', shared: 'b\'s shared'};
+        expect(ObjectUtil.inherits(a, b).shared).to.equal('a\'s shared');
+        expect(ObjectUtil.inherits(a, b).__proto__.shared).to.equal('b\'s shared');
+      });
+
+    });
+
+    describe('functions', function () {
+
+      it('creates a new object', function () {
+        var a = function () { return {a: 'a'}; };
+        var b = function () { return {b: 'b'}; };
+        expect(ObjectUtil.inherits(a, b)).to.not.equal(a);
+        expect(ObjectUtil.inherits(a, b)).to.not.equal(b);
+      });
+
+      it('makes b available in the prototype', function () {
+        var a = function () { return {a: 'a'}; };
+        var b = function () { return {b: 'b'}; };
+        expect(ObjectUtil.inherits(a, b).__proto__).to.deep.equal(b());
+      });
+
+      it('makes properties available from both objects', function () {
+        var a = function () { return {a: 'a'}; };
+        var b = function () { return {b: 'b'}; };
+        expect(ObjectUtil.inherits(a, b)).to.deep.equal({a: 'a', b: 'b'});
+      });
+
+      it('prefers properties from child object', function () {
+        var a = function () { return {a: 'a', shared: 'a\'s shared'}; };
+        var b = function () { return {b: 'b', shared: 'b\'s shared'}; };
+        expect(ObjectUtil.inherits(a, b).shared).to.equal('a\'s shared');
+        expect(ObjectUtil.inherits(a, b).__proto__.shared).to.equal('b\'s shared');
+      });
+
+    });
+
+  });
+
+});
