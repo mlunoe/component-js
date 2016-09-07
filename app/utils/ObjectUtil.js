43b157b74bd2a32a28e6df63ca88bbb37161b614
mlunoe <michael.lunoe@gmail.com>
2016-09-06 19:36:31 -0700

    Fixing comment capitalization

--- app/utils/ObjectUtil.js
+++ app/utils/ObjectUtil.js
@@ -3,9 +3,9 @@ var ObjectUtil = {
    * Polyfill from MDN
    * Copies values of all enumerable own properties from one or more source
    * objects to a target object. It will return the target object.
-   * @param  {object} target the object to copy to
-   * @param  {object} source(s) the object to copy from
-   * @return {object} the object with source properties copied to target.
+   * @param  {Object} target the object to copy to
+   * @param  {Object} source(s) the object to copy from
+   * @return {Object} the object with source properties copied to target.
    */
   assign: function (target) { // , ...source
     if (target == null) {
